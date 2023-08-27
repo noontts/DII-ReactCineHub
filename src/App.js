cb80cd26c3d92498a0aa0ae139ed952648c84154
DinnyS <118959844+DinnyS@users.noreply.github.com>
2023-08-27 08:03:43 +0700

    Feat: add router and fix navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import Container from "./components/Container";
 
-import { Navbar } from "./components/Navbar/Navbar"
+import { Navbar } from "./components/Navbar/Navbar";
+import { BrowserRouter as Router , Switch , Route} from 'react-router-dom';
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Container>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route path='/'/>
+          </Switch>
+        </>
+      </Router>
       <Container/>
     </div>
   );
