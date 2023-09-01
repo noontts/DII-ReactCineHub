866cdf6bf94160dbd0f94bea68288e2511cb49c8
DinnyS <118959844+DinnyS@users.noreply.github.com>
2023-09-01 10:02:08 +0700

    Fix : CommentDisplay

--- src/components/CommentDisplay/index.js
+++ src/components/CommentDisplay/index.js
@@ -5,9 +5,10 @@ export const CommentDisplay = () => {
     return(
         <>
             <div className='comment-containner'>
-                <h4 className='user-name'>User ท่านใด</h4>
-                <p className='comment-text'>หนังไม่สนุกครับ ป็อปคอร์นไม่อร่อย</p>
-                
+                <div className='user_data'>
+                    <h4 className='user-name'>User ท่านใด</h4>
+                    <p className='comment-text'>หนังไม่สนุกครับ ป็อปคอร์นไม่อร่อย</p>
+                </div>
                 <div className='Button-container'>
                     <button className='Edit-data'>Edit</button>
                     <button className='Delete-data'>Delete</button>
