e9b68c272916b668a1d192d7dade7b01a5ecb4e3
DinnyS <118959844+DinnyS@users.noreply.github.com>
2023-09-02 10:27:52 +0700

    Feat : add css on now-playing

--- src/pages/PaginateMovie/index.js
+++ src/pages/PaginateMovie/index.js
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from 'react'
 import MovieCard from '../../components/MovieCard'
 import { fetchData } from '../../services/movieapi'
 import { useParams, useNavigate } from 'react-router-dom';
+import { styled } from 'styled-components';
 
-const PaginatePage = ({ pageTitle, endpoint }) => {
+const PaginatePage = ({ pageTitle, endpoint , className}) => {
     const [data, setData] = useState([]);
     const navigate = useNavigate();
     const { pageNumber } = useParams();
@@ -31,16 +32,70 @@ const PaginatePage = ({ pageTitle, endpoint }) => {
     },[endpoint,pageNumber])
 
   return (
-    <>
-        <h1 style={{color: 'white'}}>{pageTitle}</h1>
-        <div style={{display: 'grid' , gridTemplateColumns: 'auto auto auto auto auto',columnGap: '35px'}}>
+    <div className={className}>
+        <h1 className='header-text' >{pageTitle}</h1>
+        <div className='show_data' style={{display: 'grid' , gridTemplateColumns: 'auto auto auto auto auto',columnGap: '35px'}}>
             {data.map((movieObject)=>(
                 <MovieCard key={movieObject.id} movieObject={movieObject}></MovieCard>
             ))}
         </div>
-        <button onClick={handleNextPage} style={{marginTop:'10%' , fontSize:'20px' , width:' 200px' , borderRadius:'12px' , cursor:'pointer'}}>Next Page</button>
-    </>
+        <button className='buttonNext' onClick={handleNextPage} >Next Page <box-icon Name='chevrons-right'></box-icon></button>
+    </div>
   )
 }
 
-export default PaginatePage
+export default styled(PaginatePage)`
+
+display: flex;
+flex-direction: column;
+justify-content: center;
+
+text-align: center;
+
+
+.header-text{
+    color: #FFF;
+    text-transform: uppercase;
+    text-shadow: 4px 4px 4px rgba(76,71,71,0.25);
+
+    margin-top: 50px;
+    margin-bottom: 10px;
+}
+
+.show_data{
+    display: grid  ;
+    grid-template-columns: auto auto auto auto auto;
+    column-gap: 35px;
+}
+
+.buttonNext{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #211f1f;
+    cursor: pointer;
+    font-size: 20px;
+    width: 200px;
+    border-radius: 12px;
+    background-color: #FFF;
+    text-shadow: 2px 2px 2px rgba(76,71,71,0.25);
+
+    margin: 0 auto;
+    margin-top: 10%;
+    transition: 300ms;
+}
+
+.buttonNext:hover{
+    background-color: #d9d9d9;
+    transition: 300ms;
+}
+
+.buttonNext:active{
+    background-color: #a1e533;
+    transition: 100ms;
+}
+
+
+
+`
+
